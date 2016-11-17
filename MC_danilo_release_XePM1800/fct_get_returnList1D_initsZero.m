c44d6d3c1636be56e9448bbc11c06abba5986dd8
Zille <Zille>
2016-11-17 18:19:37 +0100

    DELETE

--- MC_danilo_release_XePM1800/fct_get_returnList1D_initsZero.m
+++
@@ -1,6 +0,0 @@
-function [tr] = fct_get_returnList1D_initsZero(ALPHA,v,r,tgrid)
-
-
-
-
-end
--- MC_danilo_release_XePM1800/fct_get_traveltime.m
+++
@@ -1,4 +0,0 @@
-function [vpexit_ind vpexit vp_grid] = fct_get_traveltime(Eexit_sample,Ip_sample,MeanOpt,PlotOpt)
-
-
-end
